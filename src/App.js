b3294707623859e1e29a1dda17138d9de30bf6bb
NChislom <11775893+nchislom@users.noreply.github.com>
2019-07-23 18:09:40 -0400

    Added array randomizer to render() function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 
+// Ref: https://stackoverflow.com/questions/38101522/how-to-render-random-objects-from-an-array-in-react
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
@@ -13,12 +25,14 @@ class App extends Component {
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
+
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+    this.state.friends = shuffleArray(this.state.friends);
     return (
       <Wrapper>
         <Title>Friends List</Title>
