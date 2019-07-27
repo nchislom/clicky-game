b11df7e24a6b1fe6ac3185ce52de7a226fb20b72
NChislom <11775893+nchislom@users.noreply.github.com>
2019-07-27 10:13:03 -0400

    Updates

---
+++ precache-manifest.60099c77999e87e71a49cdac3fe76684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a83477c18dba4bfe736929c65dfb599",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "5c93899b922c23b1a5da",
+    "url": "/clicky-game/static/css/main.9fe1f274.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5c93899b922c23b1a5da",
+    "url": "/clicky-game/static/js/main.3c1f291b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
